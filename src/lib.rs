52944f86b1b07fcab17de1f5778b72373e08906a
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2022-09-02 18:56:37 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,117 @@
+use std::str;
+pub struct ShuntingYard {
+    stack: Vec<u8>,
+    current_index: usize,
+    operators: Vec<char>,
+    output: Vec<f64>,
+}
+
+impl ShuntingYard {
+    pub fn create(input: &str) -> ShuntingYard {
+        ShuntingYard {
+            stack: input.as_bytes().to_vec(),
+            current_index: 0,
+            operators: Vec::new(),
+            output: Vec::new(),
+        }
+    }
+
+    pub fn evaluate(&mut self) -> f64 {
+        while self.current_index < self.stack.len() {
+            let current_char: char = self.stack[self.current_index] as char;
+            match current_char {
+                '0'..='9' => {
+                    let digit: f64 = self.get_number();
+                    self.output.push(digit);
+                }
+                'a'..='d' => {
+                    if current_char == 'b' // since we are using floating point data type, value can be negative.
+                        && (self.operators.is_empty() || self.stack[self.current_index - 1] == b'e')
+                    {
+                        self.current_index += 1;
+                        let digit: f64 = -self.get_number();
+                        self.output.push(digit);
+                    } else {
+                        if self.operators.is_empty() {
+                            self.operators.push(current_char);
+                        } else {
+                            while let Some(&previous_operator) = self.operators.last() {
+                                if previous_operator == 'e'
+                                    || ((current_char == 'c' || current_char == 'd')
+                                        && (matches!(self.operators.last(), Some('a'))
+                                            || matches!(self.operators.last(), Some('b'))))
+                                {
+                                    // this is where the action happens.
+                                    // left to right.
+                                    // if we flip the contents of this block with the break keyword,
+                                    // algorithm will fail becasue it will be reading from right to left
+                                    self.operators.pop().unwrap();
+                                    self.match_and_push_to_stack(previous_operator);
+                                } else {
+                                    break;
+                                }
+                            }
+                            self.operators.push(current_char);
+                        }
+                    }
+                }
+                'e' => {
+                    self.operators.push(current_char);
+                }
+                'f' => {
+                    while let Some(previous_operator) = self.operators.pop() {
+                        if previous_operator == 'e' {
+                            break;
+                        }
+                        self.match_and_push_to_stack(previous_operator);
+                    }
+                }
+                _ => {}
+            }
+            // increment the current index
+            // to go to the next character
+            self.current_index += 1;
+        }
+        while let Some(operator) = self.operators.pop() {
+            self.match_and_push_to_stack(operator);
+        }
+        self.output.pop().unwrap()
+    }
+
+    pub fn get_number(&mut self) -> f64 {
+        let starting_pos: usize = self.current_index;
+        while self.current_index < self.stack.len() {
+            let current_char = self.stack[self.current_index] as char;
+
+            // since we are delaying with floating point integers,
+            // ensure the code doesn't break by avoiding anything with decimals
+            if !(current_char.is_digit(10) || current_char == '.') {
+                break;
+            }
+            self.current_index += 1;
+        }
+        self.current_index -= 1;
+
+        let ending_pos: usize = self.current_index;
+        unsafe { str::from_utf8_unchecked(&self.stack[starting_pos..=ending_pos]) }
+            .parse::<f64>()
+            .unwrap()
+    }
+
+    // Applies the provided operation to the last two elements on the stack.
+    // Then pushes the result on the stack.
+    pub fn match_and_push_to_stack(&mut self, op: char) {
+        let first_digit = self.output.pop().unwrap();
+        let second_digit = self.output.pop().unwrap();
+        let result = match op {
+            'a' => first_digit + second_digit,
+            'b' => first_digit - second_digit,
+            'c' => first_digit * second_digit,
+            'd' => first_digit / second_digit,
+            _ => 0.0, // @todo return empty here
+        };
+
+        // send result to output stack.
+        self.output.push(result);
+    }
+}
